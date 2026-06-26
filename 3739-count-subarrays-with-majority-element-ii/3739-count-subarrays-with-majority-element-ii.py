8265c01e2c10c6ad84af950ff372e8e3b6738833
suhanigupta786 <suhu786.gupta@gmail.com>
2026-06-26 10:58:40 +0530

    Time: 46 ms (100%), Space: 33.8 MB (25.23%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -1,44 +1,23 @@
-from typing import List
-
-class Fenwick:
-    def __init__(self, n):
-        self.bit = [0] * (n + 1)
-
-    def update(self, i, delta):
-        while i <= n:
-            self.bit[i] += delta
-            i += i & -i
-
-    def query(self, i):
-        s = 0
-        while i > 0:
-            s += self.bit[i]
-            i -= i & -i
-        return s
-
-
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
-        pref = [0]
-        cur = 0
+        n = len(nums)
+        pref = n
 
-        for x in nums:
-            cur += 1 if x == target else -1
-            pref.append(cur)
+        freq = [0] * (2 * n + 1)
+        freq[n] = 1
 
-        vals = sorted(set(pref))
-        rank = {v: i + 1 for i, v in enumerate(vals)}
-
-        global n
-        n = len(vals)
-
-        bit = Fenwick(n)
-
+        less = 0
         ans = 0
 
-        for p in pref:
-            r = rank[p]
-            ans += bit.query(r - 1)
-            bit.update(r, 1)
+        for num in nums:
+            if num == target:
+                less += freq[pref]
+                pref += 1
+            else:
+                pref -= 1
+                less -= freq[pref]
+
+            freq[pref] += 1
+            ans += less
 
         return ans
