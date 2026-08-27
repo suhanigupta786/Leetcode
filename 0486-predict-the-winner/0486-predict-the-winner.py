70ad5d08551e1ca16ac79dd23597400f7d1fef4d
suhanigupta786 <suhu786.gupta@gmail.com>
2026-08-27 21:28:23 +0530

    Refactor dynamic programming logic in winner prediction

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -5,14 +5,11 @@ class Solution:
 
         for i in range(n):
             dp[i][i] = nums[i]
-
         for length in range(2, n + 1):
             for i in range(n - length + 1):
                 j = i + length - 1
-
                 dp[i][j] = max(
                     nums[i] - dp[i + 1][j],
-                    nums[j] - dp[i][j - 1]
-                )
+                    nums[j] - dp[i][j - 1])
 
         return dp[0][n - 1] >= 0
